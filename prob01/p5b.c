76cd7ac9ce2c913bf2592630c4a7d8eb512ca4b6
Pedro Esteves <up201705160@fe.up.pt>
2019-02-25 11:52:01 +0000

    Fixed p5b.c

--- prob01/p5b.c
+++ prob01/p5b.c
@@ -2,26 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 
-int main(int argc, char* argv[], char* envp[])
+int main(int argc, char *argv[], char *envp[])
 {
     unsigned i = 0;
-    char* user = "USER";
-    char* env_var = (char*) malloc(4*sizeof(char));
 
-
-    while(envp[i] != NULL){
-        strcpy(env_var, envp[i]);
-
-        if(strcmp(user, env_var) == 0)
+    while (envp[i] != NULL)
+    {
+        if (strncmp("USER=", envp[i], 5) == 0)
             break;
 
         i++;
     }
 
-    printf("Hello %s!\n", env_var);
-
-    free(user);
-    free(env_var);
+    printf("Hello %s!\n", envp[i]+5);
 
     return 0;
-} 
+}
