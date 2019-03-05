3abbdb5cb16ad91c4689b80b6704df09124a0176
Pedro Esteves <up201705160@fe.up.pt>
2019-03-05 23:00:47 +0000

    Finished ex 3b

--- prob02/p3b.c
+++ prob02/p3b.c
@@ -15,30 +15,40 @@ int main(int argc, char *argv[])
         printf("Usage: %s <source> <destination>\n", argv[0]);
         return 1;
     }
+    
     fd1 = open(argv[1], O_RDONLY);
+    
     if (fd1 == -1)
     {
         perror(argv[1]);
         return 1;
     }
-
-    if(argc == 3){
-        fd2 = open(argv[2], O_RDONLY);
-        if(fd2 == -1){
+    
+    if (argc == 3)
+    {
+        fd2 = open(argv[2],  O_WRONLY | O_CREAT | O_EXCL, 0644);
+     
+        if (fd2 == -1)
+        {
             perror(argv[2]);
             return 1;
         }
-
-        dup2(fd2, STDOUT_FILENO);
     }
-    
+    else
+    {        
+        fd2 = dup2(fd2, STDOUT_FILENO);
+    }
+
     while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0)
-        if ((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr)
+        if ((nw = write(fd2, buffer, nr)) <= 0 || nw != nr)
         {
+            perror(argv[2]);
             close(fd1);
+            close(fd2);
             return 1;
         }
     close(fd1);
-    
+    close(fd2);
+
     return 0;
 }
