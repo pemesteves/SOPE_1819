6182e43f14bda29ac785a1c9d4ff5743a5e20a9d
Pedro Esteves <up201705160@fe.up.pt>
2019-03-05 20:02:55 +0000

    Added ex 3a and 3b. dup2 isn't working

---
+++ prob02/p3a.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFFER_SIZE 512
+
+int main(int argc, char *argv[])
+{
+    int fd1, nr, nw;
+    unsigned char buffer[BUFFER_SIZE];
+
+    if (argc != 2)
+    {
+        printf("Usage: %s <source> <destination>\n", argv[0]);
+        return 1;
+    }
+    fd1 = open(argv[1], O_RDONLY);
+    if (fd1 == -1)
+    {
+        perror(argv[1]);
+        return 1;
+    }
+    
+    while ((nr = read(fd1, buffer, BUFFER_SIZE)) > 0)
+        if ((nw = write(STDOUT_FILENO, buffer, nr)) <= 0 || nw != nr)
+        {
+            close(fd1);
+            return 1;
+        }
+    close(fd1);
+    
+    return 0;
+}
